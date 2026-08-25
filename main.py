7a7db0b6c1122f96b0fb039030c605737cd75fea
Riccardo <riccardo.lelii11@gmail.com>
2026-08-25 17:26:20 +0200

    Fix CORS for local frontend

--- main.py
+++ main.py
@@ -12,6 +12,10 @@ app.add_middleware(
     allow_origins=[
         "https://chat.tagliaetrasforma.it",
         "http://chat.tagliaetrasforma.it",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
     ],
     allow_credentials=True,
     allow_methods=["*"],
